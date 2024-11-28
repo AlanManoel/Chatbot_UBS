6c849852c16c05007efaad3997b691d4b7005e0e
Alan Manoel <alanmanoel517@gmail.com>
2024-11-28 15:54:18 -0300

    add function to save phone numbers to a file

--- client/chatbot.js
+++ client/chatbot.js
@@ -5,53 +5,77 @@ const { run } = require('../useCase/sendMessageToAI.js');
 const fs = require('fs');
 const path = require('path');
 
-const client = new Client({
-    authStrategy: new LocalAuth(), 
-    puppeteer: {
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-  });
-  
-  
-  client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
-  });
-  
-  client.on('ready', async () => {
-    console.log('O cliente está pronto!');
-  
-    try {
-      const uploadedFile = await uploadToGemini("DadosSF.pdf", "application/pdf");
-      await waitForFilesActive([uploadedFile]);
-      fs.writeFile(path.resolve(__dirname,"..", "data.txt"), `${uploadedFile.mimeType}\n${uploadedFile.uri}`, function(err) {
-        if(err) {
-            return console.log(err);
+const saveNumber = (number) => {
+  const numbersFile = path.resolve(__dirname, "..", "numbers.text");
+
+  fs.readFile(numbersFile, "utf-8", (err, data) => {
+    if (err && err.code !== 'ENOENT') {
+      console.error("Error reading file:", err);
+      return;
+    }
+    if (!data || !data.includes(number)) {
+      fs.appendFile(numbersFile, number, + "\n", (err) => {
+        if (err) {
+          return console.log(err);
+        } else {
+          console.log(`Number: ${number} was saved!`)
         }
-        console.log("The file was saved!");
-    }); 
-    } catch (error) {
-      console.error("Erro ao carregar o arquivo PDF:", error);
+      });
+    } else {
+      console.log(`Number ${number} is already saved!`);
     }
   });
-  
-  
-  client.on('message_create', async message => {
-      if (message.fromMe) return;
-    
-      console.log(message.body);
-    
-      if (message.body) {
-        const result = await run(message.body);
-        console.log(result); 
-    
-        try {
-          // client.sendMessage(message.from, 'pong'); // Para mandar mensagem pra si mesmo no WhatsApp
-          await message.reply(result); 
-        } catch (error) {
-          console.error("Erro ao enviar mensagem:", error);
-        }
+}
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+});
+
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', async () => {
+  console.log('O cliente está pronto!');
+
+  try {
+    const uploadedFile = await uploadToGemini("DadosSF.pdf", "application/pdf");
+    await waitForFilesActive([uploadedFile]);
+    fs.writeFile(path.resolve(__dirname, "..", "data.txt"), `${uploadedFile.mimeType}\n${uploadedFile.uri}`, function (err) {
+      if (err) {
+        return console.log(err);
       }
+      console.log("The file was saved!");
     });
+  } catch (error) {
+    console.error("Erro ao carregar o arquivo PDF:", error);
+  }
+});
 
 
-module.exports = {client}
+client.on('message_create', async message => {
+  if (message.fromMe) return;
+  const senderNumber = message.from;
+
+  saveNumber(senderNumber);
+
+  console.log(message.body);
+
+  if (message.body) {
+    const result = await run(message.body);
+    console.log(result);
+
+    try {
+      await message.reply(result);
+    } catch (error) {
+      console.error("Erro ao enviar mensagem:", error);
+    }
+  }
+});
+
+
+module.exports = { client }
