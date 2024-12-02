6fda3f2760af252449dbda22bc87bfbe10b79636
Alan Manoel <alanmanoel517@gmail.com>
2024-12-02 14:03:46 -0300

    Configure function to receive message as a parameter

--- useCase/sendAutomaticMessage.js
+++ useCase/sendAutomaticMessage.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-async function sendBulkMessage(client) {
+async function sendBulkMessage(client, message) {
     const fileData = await fs.promises.readFile("numbers.txt", "utf-8");
     const numbers = fileData.split("\n").map(number => number.trim()).filter(Boolean);
-    const messageText = 'Olá! Esta é uma mensagem automatizada.';
+    const messageText = message;
     for (const number of numbers) {
       try {
         await client.sendMessage(number, messageText);
