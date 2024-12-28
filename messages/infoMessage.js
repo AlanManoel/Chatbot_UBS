0a668f8f582c105756924575362713c169d0b219
Alan Manoel <alanmanoel517@gmail.com>
2024-12-28 14:53:24 -0300

    add past message

---
+++ messages/infoMessage.js
@@ -0,0 +1,7 @@
+const MESSAGE_REQUEST = "Digite o informativo que você deseja enviar para todos os números.";
+const MESSAGE_CONFIRMATION = "O informativo foi enviado para todos os números salvos.";
+
+module.exports = {
+    MESSAGE_REQUEST,
+    MESSAGE_CONFIRMATION
+}
