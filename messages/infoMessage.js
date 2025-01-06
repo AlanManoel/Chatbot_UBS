5eba6aa78874dd20d07013e7ba70a4a2a1042cd0
Alan Manoel <alanmanoel517@gmail.com>
2025-01-06 16:28:03 -0300

    Added new intro messages and name questions

--- messages/infoMessage.js
+++ messages/infoMessage.js
@@ -1,7 +1,11 @@
 const MESSAGE_REQUEST = "Digite o informativo que você deseja enviar para todos os números.";
 const MESSAGE_CONFIRMATION = "O informativo foi enviado para todos os números salvos.";
+const MESSAGE_GREETING = "Olá! Eu sou seu assistente virtual da UBS São Francisco.🤖";
+const MESSAGE_ASKNAME = "Estou à disposição para esclarecer suas dúvidas e oferecer informações que você precisar sobre a nossa UBS-SF. 💬\n\n*Mas, primeiramente, poderia me informar seu nome e sobrenome?* 🤔";
 
 module.exports = {
     MESSAGE_REQUEST,
-    MESSAGE_CONFIRMATION
+    MESSAGE_CONFIRMATION,
+    MESSAGE_GREETING,
+    MESSAGE_ASKNAME
 }
